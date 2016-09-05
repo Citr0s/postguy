a6ffb6c3e19b6aad1ce8ed4a10cae7f54c66c2a2
iLogical <iLogical8502@gmail.com>
2016-09-05 21:35:17 +0100

    Fixed Tabs
    i fixed the tab issue.

--- main.js
+++ main.js
@@ -1,58 +1,42 @@
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-const ipc = require('electron').ipcMain;
-const request = require('request');
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-var createWindow = () => {
-  // Create the browser window.
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+var ipc = require('electron').ipcMain;
+var request = require('request');
+var mainWindow;
+
+function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800
-    , height: 600
+    width: 800,
+    height: 600
   });
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-  // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.on('closed', function () {
     mainWindow = null;
   });
-};
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+}
+
 app.on('ready', createWindow);
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+
+app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+
+app.on('activate', function () {
   if (mainWindow === null) {
     createWindow();
   }
 });
-ipc.on('post', (event, arg) => {
-  request[arg.call](arg.request, (error, response, body) => {
+
+ipc.on('post', function (event, arg) {
+  request[arg.call](arg.request, function (error, response, body) {
     event.sender.send('reply', {
-      error: error
-      , response: response
-      , body: body
+      error: error,
+      response:
+      response,
+      body: body
     });
-  })
+  });
 });
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,190 +1,125 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-'use strict';
+/*globals require, ace, Vue*/
+(function () {
+  "use strict";
 
-const ipc = require('electron').ipcRenderer;
-const theme = 'github';
-const requestEditor = ace.edit("requestEditor");
-const responseEditor = ace.edit("responseEditor");
-var setupEditors = () => {
-  requestEditor.setTheme('ace/theme/' + vm.editorTheme);
-  requestEditor.getSession().setMode("ace/mode/json");
-  requestEditor.setShowPrintMargin(false);
-  requestEditor.setHighlightActiveLine(true);
-  requestEditor.resize();
-  requestEditor.setBehavioursEnabled(true);
-  requestEditor.getSession().setUseWrapMode(true);
-  responseEditor.setTheme('ace/theme/' + vm.editorTheme);
-  responseEditor.getSession().setMode("ace/mode/json");
-  responseEditor.setShowPrintMargin(false);
-  responseEditor.setHighlightActiveLine(true);
-  responseEditor.resize();
-  requestEditor.setBehavioursEnabled(true);
-  requestEditor.getSession().setUseWrapMode(true);
-};
-const vm = new Vue({
-  el: '#app',
-  data: {
-    submitting: false,
-    themes: [
-      {
-        text: 'Ambiance',
-        value: 'ambiance'
-      }, {
-        text: 'Chrome',
-        value: 'chrome'
-      }, {
-        text: 'Clouds',
-        value: 'clouds'
-      }, {
-        text: 'Clouds Midnight',
-        value: 'clouds_midnight'
-      }, {
-        text: 'Cobalt',
-        value: 'cobalt'
-      }, {
-        text: 'Crimson',
-        value: 'crimson_editor'
-      }, {
-        text: 'Dawn',
-        value: 'dawn'
-      }, {
-        text: 'Dreamweaver',
-        value: 'dreamweaver'
-      }, {
-        text: 'Eclipse',
-        value: 'eclipse'
-      }, {
-        text: 'Github',
-        value: 'github'
-      }, {
-        text: 'Idle Fingers',
-        value: 'idle_fingers'
-      }, {
-        text: 'Kr Theme',
-        value: 'kr_theme'
-      }, {
-        text: 'Merbivore',
-        value: 'merbivore'
-      }, {
-        text: 'Merbivore Soft',
-        value: 'merbivore_soft'
-      }, {
-        text: 'Mono Industrial',
-        value: 'mono_industrial'
-      }, {
-        text: 'Monokai',
-        value: 'monokai'
-      }, {
-        text: 'Solarized Light',
-        value: 'solarized_light'
-      }, {
-        text: 'Solarized Dark',
-        value: 'solarized_dark'
-      }, {
-        text: 'Textmate',
-        value: 'textmate'
-      }, {
-        text: 'Tomorrow',
-        value: 'tomorrow'
-      }, {
-        text: 'Tomorow Night',
-        value: 'tomorrow_night'
-      }, {
-        text: 'Tomorrow Night Blue',
-        value: 'tomorrow_night_blue'
-      }, {
-        text: 'Tomorrow Night Bright',
-        value: 'tomorrow_night_bright'
-      }, {
-        text: 'Tomorrow Night Eighties',
-        value: 'tomorrow_night_eighties'
-      }, {
-        text: 'Twilight',
-        value: 'twilight'
-      }, {
-        text: 'Viberant Ink',
-        value: 'viberant_ink'
-      }, {
-        text: 'Xcode',
-        value: 'xcode'
-      }],
-    editorTheme: 'monokai',
-    logs: [],
-    message: 'Postguy',
-    currentTabIndex: 0,
-    tabs: []
-  },
-  computed: {
-    headers: () => {
-      return vm.tabs[vm.currentTabIndex].request.headers.reduce((result, item) => {
-        result[item.attribute] = item.value;
-        return result;
-      }, {});
-    },
-    disableSubmit: () => {
-      return vm.tabs[vm.currentTabIndex].request.url && !vm.submitting;
-    },
-    currentTab: {
-      get: () => {
-        console.log("getting currentTab")
-        return vm.tabs[vm.currentTabIndex];
-      },
-      // setter
-      set: (newValue) => {
-        vm.tabs[vm.currentTabIndex] = newValue;
-      }
-    }
-  },
-  methods: {
-    themeEditor: (theme) => {
-      requestEditor.setTheme('ace/theme/' + theme);
-      responseEditor.setTheme('ace/theme/' + theme);
-    },
-    post: () => {
-      if (!vm.tabs[vm.currentTabIndex].request.url) return;
-      vm.submitting = true;
-      vm.tabs[vm.currentTabIndex].request.displayValue = requestEditor.getValue();
-      vm.tabs[vm.currentTabIndex].request.body = vm.tabs[vm.currentTabIndex].request.displayValue;
-      requestEditor.setValue(formatJson(vm.tabs[vm.currentTabIndex].request.displayValue));
-      responseEditor.setValue('{}');
-      ipc.send('post', {
-        call: vm.tabs[vm.currentTabIndex].request.verb,
-        request: {
-          headers: vm.headers,
-          url: vm.tabs[vm.currentTabIndex].request.url,
-          body: vm.tabs[vm.currentTabIndex].request.body
-        }
-      });
-    },
-    loadLoggedRequest: (index) => {
-      vm.tabs[vm.currentTabIndex].request = deepClone(vm.logs[index]).request;
-      vm.tabs[vm.currentTabIndex].request.headers = vm.tabs[vm.currentTabIndex].request.headers;
-      vm.response = deepClone(vm.logs[index]).response;
-      requestEditor.setValue(formatJson(vm.tabs[vm.currentTabIndex].request.displayValue));
-      responseEditor.setValue(formatJson(vm.tabs[vm.currentTabIndex].response.displayValue));
-    },
-    addHeader: () => {
-      if (!vm.newHeader.attribute || !vm.newHeader.value) return;
-      vm.tabs[vm.currentTabIndex].request.headers.push(vm.newHeader);
-      vm.newHeader = {};
-    },
-    removeHeader: (index) => {
-      vm.tabs[vm.currentTabIndex].request.headers.splice(index, 1);
-    },
-    addTab: () => {
-      vm.tabs.push({
+  var ipc = require('electron').ipcRenderer;
+  var theme = 'github';
+  var requestEditor = ace.edit("requestEditor");
+  var responseEditor = ace.edit("responseEditor");
+
+  function setupEditors() {
+    requestEditor.setTheme('ace/theme/' + vm.editorTheme);
+    requestEditor.getSession().setMode("ace/mode/json");
+    requestEditor.setShowPrintMargin(false);
+    requestEditor.setHighlightActiveLine(true);
+    requestEditor.resize();
+    requestEditor.setBehavioursEnabled(true);
+    requestEditor.getSession().setUseWrapMode(true);
+    responseEditor.setTheme('ace/theme/' + vm.editorTheme);
+    responseEditor.getSession().setMode("ace/mode/json");
+    responseEditor.setShowPrintMargin(false);
+    responseEditor.setHighlightActiveLine(true);
+    responseEditor.resize();
+    requestEditor.setBehavioursEnabled(true);
+    requestEditor.getSession().setUseWrapMode(true);
+  }
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      submitting: false,
+      themes: [
+        {
+          text: 'Ambiance',
+          value: 'ambiance'
+        }, {
+          text: 'Chrome',
+          value: 'chrome'
+        }, {
+          text: 'Clouds',
+          value: 'clouds'
+        }, {
+          text: 'Clouds Midnight',
+          value: 'clouds_midnight'
+        }, {
+          text: 'Cobalt',
+          value: 'cobalt'
+        }, {
+          text: 'Crimson',
+          value: 'crimson_editor'
+        }, {
+          text: 'Dawn',
+          value: 'dawn'
+        }, {
+          text: 'Dreamweaver',
+          value: 'dreamweaver'
+        }, {
+          text: 'Eclipse',
+          value: 'eclipse'
+        }, {
+          text: 'Github',
+          value: 'github'
+        }, {
+          text: 'Idle Fingers',
+          value: 'idle_fingers'
+        }, {
+          text: 'Kr Theme',
+          value: 'kr_theme'
+        }, {
+          text: 'Merbivore',
+          value: 'merbivore'
+        }, {
+          text: 'Merbivore Soft',
+          value: 'merbivore_soft'
+        }, {
+          text: 'Mono Industrial',
+          value: 'mono_industrial'
+        }, {
+          text: 'Monokai',
+          value: 'monokai'
+        }, {
+          text: 'Solarized Light',
+          value: 'solarized_light'
+        }, {
+          text: 'Solarized Dark',
+          value: 'solarized_dark'
+        }, {
+          text: 'Textmate',
+          value: 'textmate'
+        }, {
+          text: 'Tomorrow',
+          value: 'tomorrow'
+        }, {
+          text: 'Tomorow Night',
+          value: 'tomorrow_night'
+        }, {
+          text: 'Tomorrow Night Blue',
+          value: 'tomorrow_night_blue'
+        }, {
+          text: 'Tomorrow Night Bright',
+          value: 'tomorrow_night_bright'
+        }, {
+          text: 'Tomorrow Night Eighties',
+          value: 'tomorrow_night_eighties'
+        }, {
+          text: 'Twilight',
+          value: 'twilight'
+        }, {
+          text: 'Viberant Ink',
+          value: 'viberant_ink'
+        }, {
+          text: 'Xcode',
+          value: 'xcode'
+        }],
+      editorTheme: 'monokai',
+      logs: [],
+      message: 'Postguy',
+      currentTabIndex: 0,
+      tabs: [{
         newHeader: {},
         request: {
           verb: '',
-          headers: [{
-            attribute: 'content-type',
-            value: 'application/json; charset=utf-8'
-          }, {
-              attribute: '',
-              value: ''
-            }],
+          headers: [],
           url: '',
           body: '',
           displayValue: ''
@@ -195,37 +130,112 @@ const vm = new Vue({
           body: {},
           displayValue: ''
         }
-      });
+      }]
     },
-    removeTab: (index) => {
-      vm.tabs.splice(index, 1);
+    computed: {
+      headers: function () {
+        return this.tabs[this.currentTabIndex].request.headers.reduce(function (result, item) {
+          result[item.attribute] = item.value;
+          return result;
+        }, {});
+      },
+      disableSubmit: function () {
+        return this.tabs[this.currentTabIndex].request.url && !this.submitting;
+      }
     },
-    changeTab: (index) => {
-      vm.tabs[vm.currentTabIndex].request.displayValue = requestEditor.getValue();
-      vm.currentTabIndex = index;
-      requestEditor.setValue(vm.tabs[vm.currentTabIndex].request.displayValue || `{}`);
-      responseEditor.setValue(vm.tabs[vm.currentTabIndex].response.displayValue || `{}`);
+    methods: {
+      themeEditor: function (theme) {
+        requestEditor.setTheme('ace/theme/' + theme);
+        responseEditor.setTheme('ace/theme/' + theme);
+      },
+      post: function () {
+        if (!this.tabs[this.currentTabIndex].request.url) return;
+        this.submitting = true;
+        this.tabs[this.currentTabIndex].request.displayValue = requestEditor.getValue();
+        this.tabs[this.currentTabIndex].request.body = this.tabs[this.currentTabIndex].request.displayValue;
+        requestEditor.setValue(formatJson(this.tabs[this.currentTabIndex].request.displayValue));
+        responseEditor.setValue('{}');
+        ipc.send('post', {
+          call: this.tabs[this.currentTabIndex].request.verb,
+          request: {
+            headers: this.headers,
+            url: this.tabs[this.currentTabIndex].request.url,
+            body: this.tabs[this.currentTabIndex].request.body
+          }
+        });
+      },
+      loadLoggedRequest: function (index) {
+        this.tabs[this.currentTabIndex].request = deepClone(this.logs[index]).request;
+        this.tabs[this.currentTabIndex].request.headers = this.tabs[this.currentTabIndex].request.headers;
+        this.response = deepClone(this.logs[index]).response;
+        requestEditor.setValue(formatJson(this.tabs[this.currentTabIndex].request.displayValue));
+        responseEditor.setValue(formatJson(this.tabs[this.currentTabIndex].response.displayValue));
+      },
+      addHeader: function () {
+        if (!this.newHeader.attribute || !this.newHeader.value) return;
+        this.tabs[this.currentTabIndex].request.headers.push(this.newHeader);
+        this.newHeader = {};
+      },
+      removeHeader: function (index) {
+        this.tabs[this.currentTabIndex].request.headers.splice(index, 1);
+      },
+      addTab: function () {
+        this.tabs.push({
+          newHeader: {},
+          request: {
+            verb: '',
+            headers: [],
+            url: '',
+            body: '',
+            displayValue: ''
+          },
+          response: {
+            error: {},
+            response: {},
+            body: {},
+            displayValue: ''
+          }
+        });
+      },
+      removeTab: function (index) {
+        this.tabs.splice(index, 1);
+        if (this.tabs.length === 0)
+          this.addTab();
+      },
+      changeTab: function (index) {
+        this.tabs[this.currentTabIndex].request.displayValue = requestEditor.getValue();
+        this.currentTabIndex = index;
+        requestEditor.setValue(this.tabs[this.currentTabIndex].request.displayValue || `{}`);
+        responseEditor.setValue(this.tabs[this.currentTabIndex].response.displayValue || `{}`);
+      }
     }
-  }
-});
-ipc.on('reply', (event, arg) => {
-  vm.submitting = false;
-  vm.response = arg;
-  vm.logs.push({
-    request: deepClone(vm.tabs[vm.currentTabIndex].request),
-    response: deepClone(vm.response)
   });
-  if (vm.response.error) {
-    responseEditor.setValue(formatJson(vm.response) || '{}');
-  } else {
-    responseEditor.setValue(formatJson(vm.response.body) || '{}');
+
+  ipc.on('reply', function (event, arg) {
+    this.submitting = false;
+    this.currentTab.response = arg;
+
+    this.logs.push({
+      request: deepClone(this.tabs[this.currentTabIndex].request),
+      response: deepClone(this.response)
+    });
+
+    if (this.response.error) {
+      responseEditor.setValue(formatJson(this.response) || '{}');
+    } else {
+      responseEditor.setValue(formatJson(this.response.body) || '{}');
+    }
+
+    this.tabs[this.currentTabIndex].response.displayValue = responseEditor.getValue();
+  });
+
+  function deepClone(o) {
+    return JSON.parse(JSON.stringify(o));
   }
-  vm.tabs[vm.currentTabIndex].response.displayValue = responseEditor.getValue();
-});
-var deepClone = (o) => {
-  return JSON.parse(JSON.stringify(o));
-};
-var formatJson = (o) => {
-  return JSON.stringify(JSON.parse(o), null, 2);
-};
-setupEditors();
+
+  function formatJson(o) {
+    return JSON.stringify(JSON.parse(o), null, 2);
+  }
+
+  setupEditors();
+})();
