e329d9382fb6710f9ada1ea03fdcd7091d7d303a
Daniel Morrison <ilogical8502@gmail.com>
2016-09-09 13:51:36 +0100

    styled it up

--- frontend/ace.js
+++ frontend/ace.js
@@ -1,7 +1,7 @@
 module.exports = {
   setupEditor: (editorName, readOnly) => {
     var editor = ace.edit(editorName);
-    editor.setTheme('ace/theme/monokai');
+    editor.setTheme('ace/theme/github');
     editor.getSession().setMode("ace/mode/json");
     editor.setShowPrintMargin(false);
     editor.setHighlightActiveLine(true);
